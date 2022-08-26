0613c39915f8320c03afd31539443ae4e822601e
Viktoriia Moskalenko <vika01mos@gmail.com>
2022-08-26 20:07:51 +0300

    correction header mobile

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,20 +1,16 @@
 (() => {
   const menuBtnRef = document.querySelector("[data-menu-button]");
   const mobileMenuRef = document.querySelector("[data-menu]");
-  const menuItem = mobileMenuRef.querySelector('.site-nav__list')
 
-  function hendleMobileMenu () {
+  menuBtnRef.addEventListener("click", () => {
     const expanded =
       menuBtnRef.getAttribute("aria-expanded") === "true" || false;
 
-    menuBtnRef.classList.toggle("menu-button--is-open");
+    menuBtnRef.classList.toggle("is-open");
     menuBtnRef.setAttribute("aria-expanded", !expanded);
 
-    mobileMenuRef.classList.toggle("menu-container--is-open");
+    mobileMenuRef.classList.toggle("is-open");
 
-    document.body.classList.toggle('no-scroll');
-  };
-
-  menuBtnRef.addEventListener("click", hendleMobileMenu);
-  menuItem.addEventListener('click', hendleMobileMenu);
+    document.body.classList.toggle("modal-open");
+  });
 })();
